243f2347fbb4a2b6c5f6262d74cb14996fd8aa56
Pratik Soni <pratik.soni.5905@gmail.com>
2025-09-20 20:47:19 +0530

    erging issues

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 // add a new feature
 // this is a demo file created to work with branches in github
-<<<<<<< HEAD
 // this is a demo file created to work with branches in github
-=======
 // this is a demo file created to work with branches in github
 //just for fun
->>>>>>> 23cb0f169c063fcf8c7b32ebae6045d53831569f
